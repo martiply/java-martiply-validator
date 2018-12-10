9db03fbad251e2b16a3f382150cd6f698a9ac2cc
Martin <martin@hanatoya.jp>
2018-12-10 23:40:58 +0900

    result is now nullable

--- src/main/java/com/martiply/validator/ValidationResult.java
+++ src/main/java/com/martiply/validator/ValidationResult.java
@@ -6,7 +6,7 @@ public class ValidationResult {
     final public Validator.ValidatorEnum val;
 
     public ValidationResult(String result, Validator.ValidatorEnum val) {
-        this.result = result;
+        this.result = result.isEmpty() ? null: result;
         this.val = val;
     }
 }
